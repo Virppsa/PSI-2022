5812468a7a187399bec0bdc0424d53a5d70890b2
Virppsa <greta.virpsaite@gmail.com>
2022-09-27 17:46:36 +0300

    Added file reader class

---
+++ MailRemoverAPI/MailRemoverAPI/Controllers/fileReadTesting.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MailRemoverAPI.Util;
+
+namespace MailRemoverAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class fileReadTesting : ControllerBase
+    {
+        // GET: api/<fileReadTesting>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            FileReadClass readerClassInit = new FileReadClass();
+            readerClassInit.FileName = "json.json";
+            readerClassInit.FilePath = @"C:\Users\Greta\Source\Repos\Virppsa\PSI-2022\MailRemoverAPI\MailRemoverAPI\Controllers\json.json";
+            return new string[] { readerClassInit.readFile() };
+           
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<fileReadTesting>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<fileReadTesting>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<fileReadTesting>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<fileReadTesting>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MailRemoverAPI/MailRemoverAPI/Util/FileReadClass.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace MailRemoverAPI.Util
+{
+    public class FileReadClass
+    {
+        //Properties
+        private string fileName;
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+
+        private string filePath;
+        public string FilePath
+        {
+            get { return filePath; }
+            set { filePath = value; }
+        }
+
+        private string fileContents;
+        public string FileContents
+        {
+            set { filePath = value; }
+            get { return fileContents; }
+        }
+
+        public String readFile()
+        {
+            string text = System.IO.File.ReadAllText(FilePath);
+            return text;
+        }
+    }   
+}    
